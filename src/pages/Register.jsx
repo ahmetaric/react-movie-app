140d37bf32e8ba8fe711c6f01de6fda9026b53a9
ahmetaric <ahmet.aric1965@gmail.com>
2022-09-26 16:12:21 +0300

    v-4

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,20 +1,78 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Register = () => {
+  const [firstName, setFirstName] = useState("")
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
   return (
     <div className="d-flex justify-content-center">
       <div className="form-image d-none d-md-block">
         <img src={"https://picsum.photos/800/800"} alt="sample-movie" />
       </div>
       <div className="register-form">
-        <h1 className="form-title display-3">
-         Register
-        </h1>
-        <form id='register'>
+        <h1 className="form-title display-3">Register</h1>
+        <form id="register">
           <div className="mb-3">
-            <label htmlFor="firstName" className='form-label'>First Name</label>
-            <input type="text" className='form-control' id='firstName' placeholder='Enter your first name...' />
+            <label htmlFor="firstName" className="form-label">
+              First Name
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="firstName"
+              placeholder="Enter your first name..."
+              onChange={(e) => setFirstName(e.target.value)}
+              required
+            />
           </div>
+          <div className="mb-3">
+            <label htmlFor="lastName" className="form-label">
+              Last Name
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="lastName"
+              placeholder="Enter your last name..."
+              onChange={(e) => setLastName(e.target.value)}
+              required
+            />
+          </div>
+
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">
+              E-Mail
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              placeholder="Enter your email adress..."
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+
+          <div className="mb-3">
+            <label htmlFor="password" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              placeholder="Enter your password..."
+              onChange={(e)=>setPassword(e.target.value)}
+              required
+            />
+          </div>
+
+          <input
+            type="submit"
+            className="btn btn-primary form-control"
+            value="Register"
+          />
         </form>
       </div>
     </div>
