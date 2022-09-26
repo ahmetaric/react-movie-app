a875e512584891717794b28f1872f77830e21e3f
ahmetaric <ahmet.aric1965@gmail.com>
2022-09-26 14:42:24 +0300

    v-2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
-
+import {Link} from "react-router-dom"
 const Navbar = () => {
+  const currentUser = {displayName:"ahmet"}
   return (
-    <div>Navbar</div>
-  )
+    <div>
+      <nav className="navbar navbar-expand-lg">
+        <div className="container-fluid">
+          <Link to="/" className="navbar-brand text-white">
+            <h4>React Movie App</h4>
+          </Link>
+          <div className="d-flex text-white align-items-center">
+            {currentUser ? (
+              <>
+                <h5 className="mb-0 text-capitalize">
+                  {currentUser.displayName}
+                </h5>
+                <button className="ms-2 btn btn-outline-light">Logout</button>
+              </>
+            ) : null}
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
 }
 
 export default Navbar
