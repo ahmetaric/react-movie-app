eecccfef93c664103849c6c5308ae161dfca820b
ahmetaric <ahmet.aric1965@gmail.com>
2022-09-26 17:14:57 +0300

    v-5

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
   return (
-    <div>Login</div>
-  )
+    <div className="d-flex justify-content-center">
+      <div className="form-image d-none d-md-block">
+        <img src={"https://picsum.photos/800/800"} alt="sample-movie" />
+      </div>
+      <div className="register-form">
+        <h1 className="form-title display-3">Login</h1>
+        <form id="register" onSubmit={handleLogin}>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">
+              E-Mail
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              placeholder="Enter your email adress..."
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+
+          <div className="mb-3">
+            <label htmlFor="password" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              placeholder="Enter your password..."
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <div className="link">Forgot Password ?</div>
+          <input
+            type="submit"
+            className="btn btn-primary form-control"
+            value="Login"
+          />
+        </form>
+        <button className="btn btn-primary form-control">
+          Continue With Google
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Login
