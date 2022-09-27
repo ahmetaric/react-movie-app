8890581303cd60e567147dd9d1a818a8bb372b54
ahmetaric <ahmet.aric1965@gmail.com>
2022-09-27 17:44:47 +0300

    v-9

--- src/auth/firebase.jsx
+++ src/auth/firebase.jsx
@@ -5,6 +5,7 @@ import {
   signInWithEmailAndPassword,
   onAuthStateChanged,
   signOut,
+  updateProfile,
 } from "firebase/auth";
 
 // TODO: Replace the following with your app's Firebase project configuration
@@ -24,10 +25,14 @@ const app = initializeApp(firebaseConfig);
 // Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
 
-export const createUser = async(email, password,navigate) => {
+export const createUser = async(email, password,navigate,displayName) => {
   
    try {
     let userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    await updateProfile(auth.currentUser, {
+      displayName:displayName
+      
+    });
     navigate("/");
     console.log(userCredential);
    } catch (error) {
