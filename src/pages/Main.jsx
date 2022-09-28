25ab550d803f66f2bbe7da9320db1de3253c0015
ahmetaric <ahmet.aric1965@gmail.com>
2022-09-28 13:18:22 +0300

    v-11

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,6 +1,13 @@
+import axios from 'axios';
 import React from 'react'
+const API_KEY = process.env.REACT_APP_TMDB_KEY;
+const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
 
 const Main = () => {
+  const getMovies = (API)=>{
+    axios.get(API)
+  }
   return (
     <div>Main</div>
   )
