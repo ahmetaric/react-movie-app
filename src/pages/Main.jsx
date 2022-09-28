342dab1172c4525c4d8d6f03358c118a819a55e6
ahmetaric <ahmet.aric1965@gmail.com>
2022-09-28 14:44:39 +0300

    v-12

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,16 +1,32 @@
 import axios from 'axios';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import MovieCard from '../components/MovieCard';
 const API_KEY = process.env.REACT_APP_TMDB_KEY;
 const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
 const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
 
 const Main = () => {
+  const [movies, setMovies] = useState([])
+  const [loading, setLoading] = useState(false)
+  useEffect(() => {
+   getMovies(FEATURED_API);
+  }, [])
+  
   const getMovies = (API)=>{
-    axios.get(API)
+    setLoading(true)
+    axios.get(API).then(res=>setMovies(res.data.results)).catch(err=>console.log(err)).finally(()=>setLoading(false))
   }
   return (
-    <div>Main</div>
-  )
+    <div className="d-flex justify-content-center flex-wrap ">
+      {loading ? (
+        <div className="spinner-border text-primary m-5" role="status">
+          <span className="sr-only">Loading...</span>
+        </div>
+      ) : (
+        movies?.map((movie) => <MovieCard key={movie.id} {...movie}/>)
+      )}
+    </div>
+  );
 }
 
 export default Main
